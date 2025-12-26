0cca1f3ee4bfaf755179c55625360a340654d382
warrendeveyra <warrendeveyra11@gmail.com>
2025-12-26 17:42:55 +0800

    Updates

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Service Worker for TCG Master Set Generator
+const CACHE_NAME = 'tcg-gen-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
